c2c30c25de2db5f5f5fcbc73ff2c7feeb1b8e23c
Christoph <contact@because-why-not.com>
2024-04-15 17:23:26 +1200

    Added separate test files for call and listen

---
+++ test_call_call.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+
+from call import Call
+
+
+def main():
+    uri = os.getenv('SIGNALING_URI', 'ws://192.168.1.3:12776')
+    address = os.getenv('ADDRESS', "abc123")
+
+    call  = Call(uri)
+    loop = asyncio.get_event_loop()
+    
+    try:
+        loop.run_until_complete(call.call(address))
+    except KeyboardInterrupt:
+        pass
+    finally:        
+        print("Shutting down...")
+        loop.run_until_complete(call.dispose())
+        print("shutdown complete.")
+
+if __name__ == "__main__":
+    print("Start")
+    main()
+    
